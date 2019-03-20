6481f99501d3132ed1c1a99247e7425d9a3b87c1
PanPapag <pan_papageorgiou@hotmail.com>
2019-03-21 01:52:42 +0200

    Update in and not in

--- Python_Basics/Boolean_expressions/in_and_not_in.py
+++ Python_Basics/Boolean_expressions/in_and_not_in.py
@@ -7,3 +7,7 @@ print('ap' in "apple")      # true
 print('x' not in "apple")   # true
 
 print("a" in ["apple","lemon","orange"])    # false
+
+print("a" in ["a", "b", "c", "d"])
+print(9 in [3, 2, 9, 10, 9.0])
+print('wow' not in ['gee wiz', 'gosh golly', 'wow', 'amazing'])
