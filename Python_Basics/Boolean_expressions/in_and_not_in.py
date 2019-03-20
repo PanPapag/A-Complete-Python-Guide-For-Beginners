019c5157299cd94b6e3ff341461c1d4a38649632
PanPapag <pan_papageorgiou@hotmail.com>
2019-03-21 01:49:56 +0200

    in and not in

---
+++ Python_Basics/Boolean_expressions/in_and_not_in.py
@@ -0,0 +1,9 @@
+print('p' in "apple")       # true
+print('i' in "apple")       # false
+
+print('' in 'a')            # true (python idiomorphy)
+print('ap' in "apple")      # true
+
+print('x' not in "apple")   # true
+
+print("a" in ["apple","lemon","orange"])    # false
