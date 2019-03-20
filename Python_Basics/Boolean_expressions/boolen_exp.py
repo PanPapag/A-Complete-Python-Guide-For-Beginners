3010f1c46ac4edd547ac46074337968b3fc5f275
PanPapag <pan_papageorgiou@hotmail.com>
2019-03-21 01:40:50 +0200

    boolean expressions

---
+++ Python_Basics/Boolean_expressions/boolen_exp.py
@@ -0,0 +1,14 @@
+print(True)
+print(type(True))       # bool class
+
+print(False)
+print(type(False))      # bool class
+print(type("False"))    # str class
+
+# Evaluate expressions which use comparison operators
+print("5 = 5 ? ", 5 == 5)
+print("5 != 6 ? ", 5 != 6)
+print("3 < 4 ? ", 3 < 4)
+print("4 <= 4 ? ", 4 <= 4)
+print("100 > 200 ? ", 100 > 200)
+print("12 >= 14 ? ", 12 >= 14)
