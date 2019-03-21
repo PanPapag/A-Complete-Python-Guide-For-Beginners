d474bcb17c124baeefc6fe95ac51ff51c2fbdaa8
PanPapag <pan_papageorgiou@hotmail.com>
2019-03-21 17:44:27 +0200

    Writing to files completed

---
+++ Functions_Files_and_Dictionaries/Files/writing_file.py
@@ -0,0 +1,11 @@
+filename = "squared_numbers.txt"
+outfile = open(filename, "w")
+
+for number in range(1, 13):
+    square = number * number
+    outfile.write(str(square) + "\n")
+
+outfile.close()
+
+infile = open(filename, "r")
+print(infile.read())
