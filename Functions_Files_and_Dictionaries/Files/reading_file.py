db1861ab8ceb4c01f2b28c1adda0af863674b7c0
PanPapag <pan_papageorgiou@hotmail.com>
2019-03-21 17:37:12 +0200

    Complete reading file

--- Functions_Files_and_Dictionaries/Files/reading_file.py
+++ Functions_Files_and_Dictionaries/Files/reading_file.py
@@ -14,7 +14,27 @@ for line in lines[:4]:              # each string in list lines has a '\n' in th
 
 # more pythonic way to read whole file
 fileref.seek(0)                     # rewind fileref
-for line in fileref:
+number_of_lines = 0
+for line in fileref.readlines():
+    number_of_lines += 1
     print(line.strip("\n"))
 
 fileref.close()
+
+# Ex1 : Using the file school_prompt2.txt, find the number of characters
+#       in the file and assign that value to the variable num_char
+
+f = open("school_prompt2.txt", "r")
+content = f.read()
+num_char = len(content)
+print("school_prompt2.txt number of characters: ", num_char)
+f.close()
+
+# Ex2 : create a string called first_forty that is comprised of
+#       the first 40 characters of emotion_words2.txt.
+
+f = open("emotion_words2.txt", "r")
+content = f.read()
+first_forty = content[:40]
+print("emotion_words2.txt first 40 characters are : ", first_forty)
+f.close()
