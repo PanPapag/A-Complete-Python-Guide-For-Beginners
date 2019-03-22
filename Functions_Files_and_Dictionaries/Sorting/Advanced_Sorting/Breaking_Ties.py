4cd1dc1ff316f4582de41859dc7f8b2351079e09
PanPapag <pan_papageorgiou@hotmail.com>
2019-03-22 19:56:34 +0200

    Breaking ties completed

---
+++ Functions_Files_and_Dictionaries/Sorting/Advanced_Sorting/Breaking_Ties.py
@@ -0,0 +1,15 @@
+# Understanding tuple sorting
+tups = [('A', 3, 2),
+        ('C', 1, 4),
+        ('B', 3, 1),
+        ('A', 2, 4),
+        ('C', 1, 2)]
+for tup in sorted(tups):
+    print(tup)
+
+# Use same notion as above to sort a list having two parameters
+
+fruits = ['peach', 'kiwi', 'apple', 'blueberry', 'papaya', 'mango', 'pear']
+new_order = sorted(fruits, key=lambda fruit_name: (len(fruit_name), fruit_name))
+for fruit in new_order:
+    print(fruit)
