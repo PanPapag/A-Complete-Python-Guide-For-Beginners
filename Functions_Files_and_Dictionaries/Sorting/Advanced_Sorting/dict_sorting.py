dcc42521cfcd3239f3110195d05c1449969ec9ea
PanPapag <pan_papageorgiou@hotmail.com>
2019-03-22 19:50:05 +0200

    update dict sorting

--- Functions_Files_and_Dictionaries/Sorting/Advanced_Sorting/dict_sorting.py
+++ Functions_Files_and_Dictionaries/Sorting/Advanced_Sorting/dict_sorting.py
@@ -17,3 +17,11 @@ print("\nSorting by value")
 y = sorted(d.keys(), key=lambda k: d[k], reverse=True)
 for k in y:
     print("{} appears {} times".format(k, d[k]))
+
+# Ex : Sort the following dictionary’s keys based on the value from highest
+#       to lowest. Assign the resulting value to the variable sorted_values.
+
+dictionary = {"Flowers": 10, 'Trees': 20, 'Chairs': 6, "Firepit": 1, 'Grill': 2, 'Lights': 14}
+sorted_values = sorted(dictionary , key = lambda k: dictionary[k], reverse=True )
+print()
+print(dictionary)
