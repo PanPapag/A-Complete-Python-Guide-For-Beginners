cc8c40f664c45c84105dea1e3f6332dc283b6d8d
PanPapag <pan_papageorgiou@hotmail.com>
2019-03-22 18:29:55 +0200

    Update sorted and sort

--- Functions_Files_and_Dictionaries/Sorting_Basics/sort_and_sorted.py
+++ Functions_Files_and_Dictionaries/Sorting_Basics/sort_and_sorted.py
@@ -9,10 +9,14 @@ print(L2)
 
 L3 = ["cherry", "apple", "blueberry"]
 
-L4 = sorted(L3)
+L4 = sorted(L3) # does not mutates List
 print(L4)
 print(L3) # unchanged
 
-L3.sort()   # mutates list L3
+L3.sort()   # mutates list List
 print(L3)
 print(L3.sort())
+
+print(sorted("Apple"))
+
+# NOTE We can use sorted() in any sequence
