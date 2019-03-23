ddf2fcb2d6408b290163be253e63b876a4bee959
PanPapag <pan_papageorgiou@hotmail.com>
2019-03-23 11:14:01 +0200

    return value being an instance

---
+++ Classes_and_Inheritance/Classes/Objects_and_Instances/return_instance.py
@@ -0,0 +1,33 @@
+class Point:
+
+    def __init__(self, initX, initY):
+        self.x = initX
+        self.y = initY
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def distanceFromOrigin(self):
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+    def __str__(self):
+        return "x = {}, y = {}".format(self.x, self.y)
+
+    def halfway(self, target):
+        mx = (self.x + target.x)/2
+        my = (self.y + target.y)/2
+        return Point(mx, my)
+
+p = Point(3,4)
+q = Point(5,12)
+mid = p.halfway(q)
+# note that you would have exactly the same result if you instead wrote
+# mid = q.halfway(p)
+# because they are both Point objects, and the middle is the same no matter what
+
+print(mid)
+print(mid.getX())
+print(mid.getY())
