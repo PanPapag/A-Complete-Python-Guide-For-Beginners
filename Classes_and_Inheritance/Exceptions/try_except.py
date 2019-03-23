fe5fa756e08cf6220d40af625d5af527f3708077
PanPapag <pan_papageorgiou@hotmail.com>
2019-03-23 13:54:17 +0200

    try and except

---
+++ Classes_and_Inheritance/Exceptions/try_except.py
@@ -0,0 +1,9 @@
+try:
+    items = ['a', 'b']
+    print("This wil be printed")
+    third = items[2]
+    print("This won't print")
+except Exception:
+    print("got an error")
+
+print("continuing")
