b6a1330074d42484a14f21acd5cb27039761814a
PanPapag <pan_papageorgiou@hotmail.com>
2019-03-23 10:23:38 +0200

    exercise for practise updated

--- Classes_and_Inheritance/Classes/Constructing_Classes/constructor.py
+++ Classes_and_Inheritance/Classes/Constructing_Classes/constructor.py
@@ -16,3 +16,21 @@ point2 = Point(1,2)
 
 print("point1 ({}, {})" .format(point1.getX(),point1.getY()))
 print("point2 ({}, {})" .format(point1.getX(),point1.getY()))
+
+# EX : Create a class called Animal that accepts two numbers as inputs and
+#      assigns them respectively to two instance variables: arms and legs.
+#      Create an instance method called limbs that, when called, returns the
+#      total number of limbs the animal has. To the variable name spider,
+#      assign an instance of Animal that has 4 arms and 4 legs.
+#      Call the limbs method on the spider instance and save the result to the
+#      variable name spidlimbs.
+
+class Animal():
+    def __init__(self, arms, legs):
+        self.arms = arms
+        self.legs = legs
+    def limbs(self):
+        return self.arms + self.legs
+
+spider = Animal(4,4)
+spidlimbs = spider.limbs()
