d4e10625b30ea41195b1ec812c78cd5cf80957d5
PanPapag <pan_papageorgiou@hotmail.com>
2019-03-23 11:00:56 +0200

    Convert an object to str using __str__

---
+++ Classes_and_Inheritance/Classes/Objects_and_Instances/conv_obj_to_str.py
@@ -0,0 +1,22 @@
+class Point:
+    """ Point class for representing and manipulating x,y coordinates. """
+
+    def __init__(self, initX, initY):
+
+        self.x = initX
+        self.y = initY
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def distanceFromOrigin(self):
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+    def __str__(self):
+        return "x = {}, y = {}".format(self.x, self.y)
+
+p = Point(7,6)
+print(p)
