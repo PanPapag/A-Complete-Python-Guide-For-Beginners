06b495105699b4510fbd42d7535edc93c1800472
PanPapag <pan_papageorgiou@hotmail.com>
2019-03-23 10:57:31 +0200

    Object as arguments and parameters

---
+++ Classes_and_Inheritance/Classes/Objects_and_Instances/obj_as_arg.py
@@ -0,0 +1,28 @@
+import math
+
+class Point:
+    """ Point class for representing and manipulating x,y coordinates. """
+
+    def __init__(self, initX, initY):
+        self.x = initX
+        self.y = initY
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def distanceFromOrigin(self):
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+def distance(point1, point2):
+    xdiff = point2.getX()-point1.getX()
+    ydiff = point2.getY()-point1.getY()
+
+    dist = math.sqrt(xdiff**2 + ydiff**2)
+    return dist
+
+p = Point(4,3)
+q = Point(0,0)
+print(distance(p,q))
