7cc63ba2c3ffcee54f65fa5d76073c14254452ba
PanPapag <pan_papageorgiou@hotmail.com>
2019-03-23 10:19:44 +0200

    Minor changes

--- Classes_and_Inheritance/Classes/Constructing_Classes/constructor.py
+++ Classes_and_Inheritance/Classes/Constructing_Classes/constructor.py
@@ -15,4 +15,4 @@ point1 = Point(5,10)
 point2 = Point(1,2)
 
 print("point1 ({}, {})" .format(point1.getX(),point1.getY()))
-print("point2 ({}, {})" .format(point2.getX(),point2.getY()))
+print("point2 ({}, {})" .format(point1.getX(),point1.getY()))
