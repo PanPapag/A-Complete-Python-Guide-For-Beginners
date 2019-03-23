d0ac0c7ae2337a50447b3eb3383414a68c9286e0
PanPapag <pan_papageorgiou@hotmail.com>
2019-03-23 11:05:13 +0200

    updated

--- Classes_and_Inheritance/Classes/Objects_and_Instances/conv_obj_to_str.py
+++ Classes_and_Inheritance/Classes/Objects_and_Instances/conv_obj_to_str.py
@@ -20,3 +20,17 @@ class Point:
 
 p = Point(7,6)
 print(p)
+
+
+class Cereal:
+    def __init__(self, n, b, f):
+        self.name = n
+        self.brand = b
+        self.fiber = f
+    def __str__(self):
+        return "{} cereal is produced by {} and has {} grams of fiber in every serving!".format(self.name, self.brand, self.fiber)
+
+c1 = Cereal("Corn Flakes", "Kellogg's", 2)
+c2 = Cereal("Honey Nut Cheerios", "General Mills", 3)
+print(c1)
+print(c2)
