e083051b8ef8ab159a3348a2ec39e773b04c6e1b
PanPapag <pan_papageorgiou@hotmail.com>
2019-04-22 16:23:52 +0300

    json loads and dumps

--- Data_Collection_and_Proccessing/Nested_data_and_iteration/json_format.py
+++ Data_Collection_and_Proccessing/Nested_data_and_iteration/json_format.py
@@ -1,4 +1,5 @@
 import json
+
 a_string = '\n\n\n{\n "resultCount":25,\n "results": [\n{"wrapperType":"track", "kind":"podcast", "collectionId":10892}]}'
 print(a_string)
 d = json.loads(a_string)
@@ -6,3 +7,14 @@ print("------")
 print(type(d))
 print(d.keys())
 print(d['resultCount'])
+
+print()
+
+def pretty(obj):
+    return json.dumps(obj, sort_keys=True, indent=2)
+
+d = {'key1': {'c': True, 'a': 90, 5: 50}, 'key2':{'b': 3, 'c': "yes"}}
+
+print(d)
+print('--------')
+print(pretty(d))
