9e3e7aecb67b4a48579c7f39079d6028729f6a5e
PanPapag <pan_papageorgiou@hotmail.com>
2019-04-22 16:48:10 +0300

    nested iteration

---
+++ Data_Collection_and_Proccessing/Nested_data_and_iteration/nested_iter.py
@@ -0,0 +1,14 @@
+nested1 = [['a', 'b', 'c'],['d', 'e'],['f', 'g', 'h']]
+for x in nested1:
+    print("level1: ")
+    for y in x:
+        print("     level2: " + y)
+
+nested1 = [1, 2, ['a', 'b', 'c'],['d', 'e'],['f', 'g', 'h']]
+for x in nested1:
+    print("level1: ")
+    if type(x) is list:
+        for y in x:
+            print("     level2: {}".format(y))
+    else:
+        print(x)
