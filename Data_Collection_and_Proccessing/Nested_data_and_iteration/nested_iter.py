301ffebb9bea3ed78d2156d9ffb4e5de95b12682
PanPapag <pan_papageorgiou@hotmail.com>
2019-04-22 16:49:02 +0300

    structuring nested data

--- Data_Collection_and_Proccessing/Nested_data_and_iteration/nested_iter.py
+++ Data_Collection_and_Proccessing/Nested_data_and_iteration/nested_iter.py
@@ -4,8 +4,10 @@ for x in nested1:
     for y in x:
         print("     level2: " + y)
 
-nested1 = [1, 2, ['a', 'b', 'c'],['d', 'e'],['f', 'g', 'h']]
-for x in nested1:
+print("\n")
+
+nested2 = [1, 2, ['a', 'b', 'c'],['d', 'e'],['f', 'g', 'h']]
+for x in nested2:
     print("level1: ")
     if type(x) is list:
         for y in x:
