6620c311b1867bb4ce3aa3bbfc89a543b14e39c7
PanPapag <pan_papageorgiou@hotmail.com>
2019-04-22 16:12:11 +0300

    nested dict completed

---
+++ Data_Collection_and_Proccessing/Nested_data_and_iteration/nested_dict.py
@@ -0,0 +1,17 @@
+#EX: Extract the value associated with the key color and assign it to the variable color. Do not hard code this.
+info = {'personal_data':
+         {'name': 'Lauren',
+          'age': 20,
+          'major': 'Information Science',
+          'physical_features':
+             {'color': {'eye': 'blue',
+                        'hair': 'brown'},
+              'height': "5'8"}
+         },
+       'other':
+         {'favorite_colors': ['purple', 'green', 'blue'],
+          'interested_in': ['social media', 'intellectual property', 'copyright', 'music', 'books']
+         }
+      }
+      
+color = info['personal_data']['physical_features']['color']
