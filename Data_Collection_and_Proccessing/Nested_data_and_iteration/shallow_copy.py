9c84cc70304532dd731e9f2cff0c548225859c94
PanPapag <pan_papageorgiou@hotmail.com>
2019-04-23 10:16:22 +0300

    shallow copy

---
+++ Data_Collection_and_Proccessing/Nested_data_and_iteration/shallow_copy.py
@@ -0,0 +1,9 @@
+original = [['dogs', 'puppies'], ['cats', "kittens"]]
+copied_version = original[:]
+print(copied_version)
+print(copied_version is original)
+print(copied_version == original)
+original[0].append(["canines"])
+print(original)
+print("-------- Now look at the copied version -----------")
+print(copied_version)
