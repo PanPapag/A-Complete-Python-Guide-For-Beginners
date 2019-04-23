7c3b9b4a159e79167a18bb167ccc911585ae3df7
PanPapag <pan_papageorgiou@hotmail.com>
2019-04-23 10:23:33 +0300

    copy completed

--- Data_Collection_and_Proccessing/Nested_data_and_iteration/deep_copy.py
+++ Data_Collection_and_Proccessing/Nested_data_and_iteration/deep_copy.py
@@ -10,3 +10,34 @@ original[0].append(["canines"])
 print(original)
 print("-------- Now look at the copied version -----------")
 print(copied_outer_list)
+
+# Or, equivalently, you could take advantage of the slice
+# operator to do the copying of the inner list.
+print("A better way to perform deep copy")
+
+original = [['dogs', 'puppies'], ['cats', "kittens"]]
+copied_outer_list = []
+for inner_list in original:
+    copied_inner_list = inner_list[:]
+    copied_outer_list.append(copied_inner_list)
+print(copied_outer_list)
+original[0].append(["canines"])
+print(original)
+print("-------- Now look at the copied version -----------")
+print(copied_outer_list)
+
+# using module copy
+
+print("Using module copy ")
+import copy
+original = [['canines', ['dogs', 'puppies']], ['felines', ['cats', 'kittens']]]
+shallow_copy_version = original[:]
+deeply_copied_version = copy.deepcopy(original)
+original.append("Hi there")
+original[0].append(["marsupials"])
+print("-------- Original -----------")
+print(original)
+print("-------- deep copy -----------")
+print(deeply_copied_version)
+print("-------- shallow copy -----------")
+print(shallow_copy_version)
