d37da5cd09efb64cdaac61a2e183263ac429844c
PanPapag <pan_papageorgiou@hotmail.com>
2019-05-01 12:26:21 +0300

    ex2 completed

--- Data_Collection_and_Proccessing/Map_and_Filter/map.py
+++ Data_Collection_and_Proccessing/Map_and_Filter/map.py
@@ -26,3 +26,12 @@ lst = [["hi", "bye"], "hello", "goodbye", [9, 2], 4]
 greeting_doubled = list(map(lambda value: 2*value, lst))
 print(lst)
 print(greeting_doubled)
+
+#EX2: Below, we have provided a list of strings called abbrevs.
+# Use map to produce a new list called abbrevs_upper that contains all
+# the same strings in upper case.
+
+abbrevs = ["usa", "esp", "chn", "jpn", "mex", "can", "rus", "rsa", "jam"]
+abbrevs_upper = list(map(lambda value: value.upper(), abbrevs))
+print(abbrevs)
+print(abbrevs_upper)
