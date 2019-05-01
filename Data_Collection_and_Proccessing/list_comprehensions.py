6926acfc2323724494c8b79b750d7e4a330d18fc
PanPapag <pan_papageorgiou@hotmail.com>
2019-05-01 13:01:55 +0300

    ex2 completed

--- Data_Collection_and_Proccessing/list_comprehensions.py
+++ Data_Collection_and_Proccessing/list_comprehensions.py
@@ -18,3 +18,32 @@ print(list(map(lambda x: x*2, filter(lambda y: y % 2 == 0, things))))
 
 # equivalent version using list comprehension
 print([x*2 for x in things if x % 2 == 0])
+
+"""
+The for loop below produces a list of numbers greater than 10. Below the
+given code, use list comprehension to accomplish the same thing.
+Assign it the the variable lst2. Only one line of code is needed.
+"""
+
+L = [12, 34, 21, 4, 6, 9, 42]
+lst = []
+for x in L:
+    if x > 10:
+        lst.append(x)
+print(lst)
+
+lst2 = [x for x in L if x > 10]
+print(lst2)
+
+"""
+Write code to assign to the variable compri all the values of the key name
+in any of the sub-dictionaries in the dictionary tester. Do this using a
+list comprehension.
+"""
+
+tester = {'info': [{"name": "Lauren", 'class standing': 'Junior', 'major': "Information Science"},{'name': 'Ayo', 'class standing': "Bachelor's", 'major': 'Information Science'}, {'name': 'Kathryn', 'class standing': 'Senior', 'major': 'Sociology'}, {'name': 'Nick', 'class standing': 'Junior', 'major': 'Computer Science'}, {'name': 'Gladys', 'class standing': 'Sophomore', 'major': 'History'}, {'name': 'Adam', 'major': 'Violin Performance', 'class standing': 'Senior'}]}
+import json
+inner_list = tester['info']
+print(json.dumps(inner_list, indent=2))
+compri = [d['name'] for d in inner_list]
+print(compri)
